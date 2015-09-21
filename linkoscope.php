a9fd833cf8c6676cb4e0bc9967ccbca222ea6e29
Nabeel Sulieman <nabsul@outlook.com>
2015-09-21 10:05:38 -0400

    added main plugin file.

---
+++ linkoscope.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name: Linkoscope Post Type
+ * Version 0.1
+ */
+
+function linkoscope_post_type_init () {
+	$supports = array('title',
+		'editor',
+		'author',
+		'content',
+		'excerpt',
+		'comments',
+		'page-attributes',
+		'custom-fields',
+	);
+
+	$typeArgs = array(
+		'label'               => __('Linkoscope Links'),
+		'public'              => true, // if false, it won't show in WP-API
+		'show_in_rest'        => true, // this has to be set for it to have a route
+		'rest_base'           => 'linkolink', // api root: `/wp/v2/linkolink`
+		'publicly_queryable'  => true,
+		'exclude_from_search' => false,
+		'show_in_nav_menus'   => false,
+		'show_ui'             => false,
+		'supports'            => $supports,
+		'capability_type'     => 'post',
+	);
+	register_post_type( 'linkoscope_link', $typeArgs );
+}
+
+add_action('init', 'linkoscope_post_type_init');
